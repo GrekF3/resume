4afb720002a8a4659ee2ffc979d1332b12ebf950
GrekF3 <v.beats@inbox.ru>
2022-10-08 16:21:16 +0300

    Новые иконки и ссылки на соц.сети

--- resume/main/forms.py
+++ resume/main/forms.py
@@ -3,19 +3,21 @@ from .models import ContactProfile
 
 
 class ContactForm(forms.ModelForm):
+
+    class Meta:
+        model = ContactProfile
+        fields = '__all__'
+
     name = forms.CharField(max_length=100, required=True,
                            widget=forms.TextInput(attrs={
-                               'placeholder': '*Full name..',
-                               'class': 'form-control'
+                               'placeholder': '*Ваше имя..',
                            }))
     email = forms.EmailField(max_length=254, required=True,
                              widget=forms.TextInput(attrs={
-                                 'placeholder': '*Email..',
-                                 'class': 'form-control'
+                                 'placeholder': '*Почта..',
                              }))
     message = forms.CharField(max_length=1000, required=True,
                               widget=forms.Textarea(attrs={
-                                  'placeholder': '*Message..',
-                                  'class': 'form-control',
+                                  'placeholder': '*Сообщение..',
                                   'rows': 6,
                               }))
